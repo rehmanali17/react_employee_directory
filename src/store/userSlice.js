bf3fb20fab2c6e18ce546b6ed617505e3e5bc6cd
rehmanali17 <rehmanmalik17@gmail.com>
2022-07-08 18:06:27 +0500

    Fixed redirect URI issue

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -102,6 +102,7 @@ export const addUser = values => async dispatch => {
         const { data: user, error } = await supabase.auth.api.inviteUserByEmail(
             values.email,
             {
+                redirectTo: `${process.env.REACT_APP_URL}/signup`,
                 data: { role: values.role },
             }
         );
